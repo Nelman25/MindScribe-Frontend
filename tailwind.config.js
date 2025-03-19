fee08de1476e1bee7d117b5c3bf146b7ed030ff2
Nelman25 <jonelvillaver735@gmail.com>
2025-03-19 20:47:38 +0800

    add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#4a6fa5",
+        pl: "#e0e8f5",
+        text: "#333333",
+        "text-light": "#777777",
+        background: "#f9f9f9",
+        card: "#ffffff",
+        border: "#e0e0e0",
+      },
+    },
   },
   plugins: [],
 };
