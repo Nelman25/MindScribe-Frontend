04cb3fcf4f81bdc496e8556cf781cc7c2fc91ced
Nelman25 <jonelvillaver735@gmail.com>
2025-03-19 16:13:29 +0800

    set up

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAbyC91-INa_y_7ghUOqmbh36m7V5BNQ-I",
+  authDomain: "mindscribe-40af0.firebaseapp.com",
+  projectId: "mindscribe-40af0",
+  storageBucket: "mindscribe-40af0.firebasestorage.app",
+  messagingSenderId: "792348999156",
+  appId: "1:792348999156:web:728c01c73b592dc39e0da1",
+  measurementId: "G-W7QX5C42G5",
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
