8a20c5480d28b82c97f03c0bb8d1dfa625dc5633
Nelman25 <jonelvillaver735@gmail.com>
2025-03-21 08:52:59 +0800

    extract filter button component

---
+++ src/components/FilterButton.tsx
@@ -0,0 +1,7 @@
+export default function FilterButton({ children }: { children: string }) {
+  return (
+    <button className="px-[16px] py-[6px] bg-white border rounded-2xl hover:text-primary hover:bg-pl hover:border-primary transition-all duration-200">
+      {children}
+    </button>
+  );
+}
