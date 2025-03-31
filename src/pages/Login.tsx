64b9088170ae6b22afa70c122acb7f9b386c7271
Nelman25 <jonelvillaver735@gmail.com>
2025-03-31 23:06:53 +0800

    Login feature done

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -3,10 +3,104 @@ import Footer from "../sections/Footer";
 
 import { FcGoogle } from "react-icons/fc";
 import { IoLogoFacebook } from "react-icons/io5";
-import { FaXTwitter } from "react-icons/fa6";
-import { Link } from "react-router-dom";
+import { FaGithub } from "react-icons/fa";
+
+import { Link, useNavigate } from "react-router-dom";
+import { signInWithEmailAndPassword, signInWithPopup } from "firebase/auth";
+import {
+  auth,
+  facebookProvider,
+  githubProvider,
+  googleProvider,
+} from "../firebase";
+import { handleAuthError } from "../utils/authHelpers";
+import React, { useState } from "react";
+import { Credentials } from "../types";
+import AuthProviderButton from "../components/AuthProviderButton";
+import { VscLoading } from "react-icons/vsc";
 
 export default function Login() {
+  const [credentials, setCredentials] = useState<Credentials>({
+    email: "",
+    password: "",
+  });
+  const [errors, setErrors] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const navigate = useNavigate();
+
+  const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setCredentials((prev) => ({ ...prev, email: e.target.value }));
+  };
+
+  const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setCredentials((prev) => ({ ...prev, password: e.target.value }));
+  };
+
+  const handleLogin = async (): Promise<void> => {
+    try {
+      setLoading(true);
+      await signInWithEmailAndPassword(
+        auth,
+        credentials.email,
+        credentials.password
+      );
+      setLoading(false);
+      navigate("/dashboard", { replace: true });
+    } catch (error) {
+      setLoading(false);
+      let errorMessage = "An unknown error occured: ";
+
+      if (error instanceof Error) {
+        errorMessage += error.message;
+      } else if (typeof error === "string") {
+        errorMessage += error;
+      } else if (error && typeof error === "object" && "message" in error) {
+        errorMessage += String(error.message);
+      }
+      setErrors(errorMessage);
+      // handleAuthError(error);
+    }
+  };
+
+  const handleSignInWithGoogle = async (): Promise<void> => {
+    setLoading(true);
+    try {
+      await signInWithPopup(auth, googleProvider);
+      setLoading(false);
+      navigate("/dashboard", { replace: true });
+    } catch (error) {
+      setLoading(false);
+      if (error instanceof Error) setErrors(error.message);
+      handleAuthError(error);
+    }
+  };
+
+  const handleSignInWithFacebook = async (): Promise<void> => {
+    setLoading(true);
+    try {
+      await signInWithPopup(auth, facebookProvider);
+      setLoading(false);
+      navigate("/dashboard", { replace: true });
+    } catch (error) {
+      if (error instanceof Error) setErrors(error.message);
+      handleAuthError(error);
+    }
+  };
+
+  const handleSignInWithGithub = async (): Promise<void> => {
+    setLoading(true);
+    try {
+      await signInWithPopup(auth, githubProvider);
+      setLoading(false);
+      navigate("/dashboard", { replace: true });
+    } catch (error) {
+      setLoading(false);
+      if (error instanceof Error) setErrors(error.message);
+      handleAuthError(error);
+    }
+  };
+
   return (
     <div className="flex flex-col w-[100%] h-screen bg-background dots to-white">
       <header className="h-[70px] sticky top-0 flex bg-white items-center px-8 border shadow">
@@ -16,7 +110,7 @@ export default function Login() {
       </header>
 
       <main className="flex flex-1 items-center justify-center">
-        <div className="flex max-w-[900px] w-full h-[700px] max-xl:max-w-[700px] max-lg:flex-col max-lg:h-auto max-lg:my-8 max-lg:max-w-[600px] bg-white rounded-lg overflow-hidden shadow-lg">
+        <div className="flex max-w-[900px] w-full h-[820px] max-xl:max-w-[700px] max-lg:flex-col max-lg:h-auto max-lg:my-8 max-lg:max-w-[600px] bg-white rounded-lg overflow-hidden shadow-lg">
           <div className="max-lg:w-full w-1/2 bg-primary  h-full px-8 flex flex-col gap-4 items-center justify-center">
             <h2 className="max-lg:py-4 text-white text-3xl text-center font-medium">
               Welcome to MindScribe
@@ -37,8 +131,10 @@ export default function Login() {
                 Email
               </label>
               <input
-                type="text"
+                type="email"
                 placeholder="Email or phone number"
+                value={credentials.email}
+                onChange={handleEmailChange}
                 className="border p-4 rounded-xl text-text bg-background mb-4 focus:outline-primary focus:bg-white transition-all duration-500"
               />
             </div>
@@ -50,6 +146,8 @@ export default function Login() {
               <input
                 type="password"
                 placeholder="Password"
+                value={credentials.password}
+                onChange={handlePasswordChange}
                 className="border p-4 rounded-xl text-text bg-background mb-4 focus:outline-primary focus:bg-white transition-all duration-500"
               />
             </div>
@@ -59,8 +157,18 @@ export default function Login() {
               <span className="px-2 text-lg">Remember me</span>
             </div>
 
-            <Button type="default" className="w-full">
-              Sign In
+            {errors && <p className="text-red-500 text-sm">{errors}</p>}
+
+            <Button
+              onClick={handleLogin}
+              type="default"
+              className="w-full flex justify-center"
+            >
+              {loading ? (
+                <VscLoading className="size-6 animate-spin" />
+              ) : (
+                "Sign In"
+              )}
             </Button>
 
             <div className="flex items-center justify-between">
@@ -70,15 +178,27 @@ export default function Login() {
             </div>
 
             <div className="grid grid-cols-3 my-2 gap-4">
-              <div className="p-3 rounded-xl border bg-white hover:bg-pl hover:border-primary hover:-translate-y-[1px] transition-all duration-200">
-                <FcGoogle className="size-6 mx-auto" />
-              </div>
-              <div className="p-3 rounded-xl border bg-white hover:bg-pl hover:border-primary hover:-translate-y-[1px] transition-all duration-200">
-                <IoLogoFacebook className="size-6 text-[#1877F2] mx-auto" />
-              </div>
-              <div className="p-3 rounded-xl border bg-white hover:bg-pl hover:border-primary hover:-translate-y-[1px] transition-all duration-200">
-                <FaXTwitter className="size-6 mx-auto" />
-              </div>
+              <AuthProviderButton onClick={handleSignInWithGoogle}>
+                {loading ? (
+                  <VscLoading className="size-4 animate-spin" />
+                ) : (
+                  <FcGoogle className="size-6 mx-auto" />
+                )}
+              </AuthProviderButton>
+              <AuthProviderButton onClick={handleSignInWithFacebook}>
+                {loading ? (
+                  <VscLoading className="size-4 animate-spin" />
+                ) : (
+                  <IoLogoFacebook className="size-6 text-blue-500 mx-auto" />
+                )}
+              </AuthProviderButton>
+              <AuthProviderButton onClick={handleSignInWithGithub}>
+                {loading ? (
+                  <VscLoading className="size-4 animate-spin" />
+                ) : (
+                  <FaGithub className="size-6 mx-auto" />
+                )}
+              </AuthProviderButton>
             </div>
             <span className="text-text block text-center py-4">
               Don't have an account?
